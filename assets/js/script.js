f5a2db57bc017740423141e688336f28d5e5505e
MiaAntunes <suportecamilaantunes2003@gmail.com>
2023-04-28 12:36:58 -0300

    Verificação do Formulário - DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,57 @@
-const inputName = document.querySelector("#input-name")
-const inputFone = document.querySelector("#input-fone")
-const inputEmail = document.querySelector("#input-email")
+let inputName = document.querySelector("#input-name")
+let inputPhone = document.querySelector("#input-fone")
+let inputEmail = document.querySelector("#input-email")
 
-let respostaVerificacao = document.querySelector("#verificacao")
+let responseValidation = document.querySelector("#verificacao")
 
-function enviarFormulario(event) {
-    let validacaoDosInput = []
-    
-    //Nome (refazer !!!!!)
-    if( typeof inputName.value === "string" ){
-        validacaoDosInput.push(true);
-        console.log("Certo")
-    }else{
-        validacaoDosInput.push(false);
-        console.log("Errado")
+let validationOfInput = []
+
+function resultForm(event) {
+    event.preventDefault()
+
+    if (event) {
+        //------ Lógica de Verificação do Nome ------------
+        const name = inputName.value
+        if (name.length > 3) {
+            validationOfInput.push(true);
+        } else {
+            validationOfInput.push(false);
+        }
+
+
+        // ------ Lógica de Verificação do Telefone ------------
+        const phone = inputPhone.value
+        if (phone.length >= 10) {
+            validationOfInput.push(true);
+
+        } else {
+            validationOfInput.push(false);
+        }
+
+
+        // ------ Lógica de Verificação do Email ------------
+        const email = inputEmail.value
+        const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+
+        if (regexEmail.test(email)) {
+            validationOfInput.push(true);
+        } else {
+            validationOfInput.push(false);
+        }
+
+        // ------ Lógica de Verificação de Todos os Inputs e a incerção da Mensagem ------------
+        if (validationOfInput[0] === true && validationOfInput[1] === true && validationOfInput[2] === true) {
+            responseValidation.innerHTML = "Dados Enviados! Nossos colaboradores irão entrar em contato com você"
+        } else {
+            responseValidation.innerHTML = "Dados Inválidos!!"
+        }
+
+        // Limpar os Inputs
+        inputName.value = " ";
+        inputPhone.value = " ";
+        inputEmail.value = " ";
     }
 
-    // Fone OKKKKKKKKKKKKKKKKKKKK -------
-    const fone = inputFone.value
-    if( fone.length < 10){
-        console.log("Errado")
-        validacaoDosInput.push(false);
-
-    }else{
-        console.log("Certo")
-        validacaoDosInput.push(true);
-    }
-
-    //Email - Não feito
-
-    if(inputEmail.value === "" ){
-
-    }else{
-
-    }
-
-    //Verificação total
-    if(validacaoDosInput[0] === true && validacaoDosInput[1] === true && validacaoDosInput[2] === true){
-        respostaVerificacao.innerHTML = "Dados Enviados! Nossos colaboradores irão entrar em contato com você"
-    }else{
-        respostaVerificacao.innerHTML = "Dados Inválidos!!"
-    }
-
-    console.log(validacaoDosInput)
 }
+
+
